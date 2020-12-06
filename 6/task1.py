ea48c89e8ad1eaa5224fb442ad2a185389fd124f
Taavi Repän <trepan@gmail.com>
2020-12-06 20:12:48 +0100

    Comments for today's solution.

--- 6/task1.py
+++ 6/task1.py
@@ -3,6 +3,8 @@ yes = set()
 for line in open("input.txt"):
 	line = line.strip()
 	if line == "":
+		# Note that for this data, I added one more line at the end of input.txt, so
+		# that this if is always entered at the end of the file.
 		print(yes)
 		ret += len(yes)
 		yes = set()
