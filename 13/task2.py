74d024b2ac43188841eb15e5fa9096a7cc9f0512
Taavi Repän <trepan@gmail.com>
2020-12-13 11:57:34 +0100

    Some comments

--- 13/task2.py
+++ 13/task2.py
@@ -13,7 +13,6 @@ for i,bus in enumerate(input().split(",")):
 
 def check(i):
 	ok = True
-	print(i, i%buses == ofs, i%buses, ofs)
 	for j,b in zip(ofs, buses):
 		if (i+j)%b != 0:
 			ok = False
@@ -25,42 +24,14 @@ def check(i):
 buses = np.array(buses)
 ofs = np.array(ofs)%buses
 
-# idx = np.argsort(buses)
-# buses = buses[idx]
-# ofs = ofs[idx]
-
-print(buses)
-print(ofs)
 
 t = 0
 x = 1
 for i in range(0, len(buses)):
-	print(f"i={i} t={t} x={x} : {t}%{buses[i]} {t%buses[i]} != {ofs[i]}")
 
 	while t%buses[i] != ofs[i]:
 		t += x
-	print(t%buses-ofs, f"({t})")
-	# print(ofs)
-	print()
-	# x = np.lcm(x, buses[i])
 	x = x*buses[i]
 
-print(f"x={x}")
-print(f"t={t}")
-# print(x-t + ofs.max())
-print(buses)
-print(ofs)
 
-for i in range(0, ofs.max()+1):
-	check(x-t+i)
-	# assert x-t+i != 1068781
-		          # 1068781
-check(t)
-# print(t, t+x)
-# print("RUNNING")
-# while True:
-	# check(t)
-	# t += x
-
-# 1068781
-# 3162341
+check(x-t) # no clue why x-t, but it works, so ¯\_(ツ)_/¯
