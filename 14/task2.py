c1077efd81caa173425b71fa7d84f916a0303dae
Taavi Repän <trepan@gmail.com>
2020-12-14 08:28:54 +0100

    cleanups

--- 14/task2.py
+++ 14/task2.py
@@ -4,7 +4,6 @@ import re
 def write(mem, a, v, pmask):
 	if len(pmask) == 0:
 		mem[a] = v
-		# print(bin(a|2**33),v)
 	else:
 		*pmask, l = pmask
 		write(mem, a, v, pmask)
@@ -18,21 +17,14 @@ for line in open("/dev/stdin"):
 		omask = int(mask.replace("X","0"), 2)
 		amask = int(mask.replace("0","1").replace("X","0"), 2)
 		pmask = [len(mask)-1-i for i,x in enumerate(mask) if x == "X"]
-		# print(mask, pmask, amask, omask)
 	else:
 		m = re.match(r"mem\[(\d+)\] = (\d+)", line)
 		a = int(m[1])
 		v = int(m[2])
-		# print(bin(a|2**33))
 		a = a&amask|omask
-		# print("write")
-		# print(bin(a|2**33), v, "*")
 		write(mem, a, v, pmask)
-		# print("done")
 r = 0
 for v in mem.values():
 	r += v
 
 print(r)
-print(r > 468487937448)
-print(r > 1358338157334)
