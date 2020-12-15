60fb3171f6e263259ad44600b1b80050feb9a76a
Taavi Repän <trepan@gmail.com>
2020-12-15 07:33:13 +0100

    today

---
+++ 15/task1.py
@@ -0,0 +1,24 @@
+import itertools
+
+nrs = list(map(int,input().split(",")))
+
+spoken = dict()
+age = dict()
+for i,nr in enumerate(nrs):
+	if nr in spoken:
+		age[nr] = spoken[nr]
+	spoken[nr] = i
+	# print("*",nr)
+
+for i in range(i+1, 2020):
+	if nr not in age:
+		nr = 0
+	else:
+		# print(f"{nr}: {spoken[nr]} {age[nr]}")
+		nr = spoken[nr] - age[nr]
+	
+	if nr in spoken:
+		age[nr] = spoken[nr]
+	spoken[nr] = i
+	# print(i, nr)
+print(nr)
